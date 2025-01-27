d52981771491cb5354b3b10d39fb54b5464f6269
andres garcia <andreswgt1990@gmail.com>
2025-01-27 00:35:05 -0300

    se inicia proyecto crm

---
+++ api-crm-erp/app/Http/Controllers/AuthController.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use Symfony\Component\Process\Exception\RuntimeException;
+use Tymon\JWTAuth\JWTGuard;
+
+class AuthController extends Controller
+{
+    /**
+     * Create a new AuthController instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth:api', ['except' => ['login', 'register']]);
+    }
+ 
+ 
+    /**
+     * Register a User.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function register() {
+        $validator = Validator::make(request()->all(), [
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:8',
+        ]);
+ 
+        if($validator->fails()){
+            return response()->json($validator->errors()->toJson(), 400);
+        }
+ 
+        $user = new User;
+        $user->name = request()->name;
+        $user->email = request()->email;
+        $user->password = bcrypt(request()->password);
+        $user->save();
+ 
+        return response()->json($user, 201);
+    }
+ 
+ 
+    /**
+     * Get a JWT via given credentials.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function login()
+    {
+        $credentials = request(['email', 'password']);
+ 
+        if (! $token = auth('api')->attempt($credentials)) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+ 
+        return $this->respondWithToken($token);
+    }
+ 
+    /**
+     * Get the authenticated User.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function me()
+    {
+        return response()->json(auth('api')->user());
+    }
+ 
+    /**
+     * Log the user out (Invalidate the token).
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function logout()
+    {
+        auth('api')->logout();
+ 
+        return response()->json(['message' => 'Successfully logged out']);
+    }
+ 
+    /**
+     * Refresh a token.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function refresh()
+    {
+        return $this->respondWithToken(JWTAuth::refresh());
+    }
+ 
+    /**
+     * Get the token array structure.
+     *
+     * @param  string $token
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function respondWithToken($token)
+    {
+        $guard = Auth('api');
+        if (! $guard instanceof JWTGuard) {
+            throw new RuntimeException('Wrong guard returned.');
+        }
+
+        return response()->json([
+            'token_type' => 'Bearer',
+            'access_token' => $guard->refresh(),
+            'expires_in' => $guard->factory()->getTTL() * 60,
+            'user' =>[
+                "full_name" => $guard->user()->name.' '.$guard->user()->surname,
+                "email" => $guard->user()->email,
+                //avatar
+            ]
+        ]);
+    }
+}
